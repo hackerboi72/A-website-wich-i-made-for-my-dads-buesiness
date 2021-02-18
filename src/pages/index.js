d6fb9bbdda756c5d64031479f99a2411d6a3be2d
Russian hacker boi <77713906+hackerboi72@users.noreply.github.com>
2021-02-18 22:00:17 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { Override, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Welcome to Ascent Heath Clinic!!!
+			</Text>
+			<Text as="p" font="--headline3" margin="20px 0 0 0">
+				Hi There!! My name is Dr . Ankit Jain!!. I am an ENT Surgeon. Our clinic is located at Ghaziabad. To book an appointment, Call this number- 9971074350. If you don't want an appointment for the clinic, you can book an appointment for Sant Parmanand Hospital using the same number. For any doubts contact us at ascentheathclinic@gmail.com
+			</Text>
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Text
+				font="--base"
+				text-transform="uppercase"
+				letter-spacing="1px"
+				max-width="850px"
+				color="--grey"
+				margin="0px"
+			>
+				Our Mission
+			</Text>
+			<Text
+				as="h1"
+				margin="8px 0px 32px"
+				font="--headline1"
+				md-font="--headline2"
+				color="--dark"
+				max-width="850px"
+			>
+				Our mission is to help anyone with problems or illness of ear, nose and throat.{" "}
+			</Text>
+		</Section>
+		<Box />
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"602e93f58db767001e87948b",
+				"602e93f58db767001e87948d"
+			]
+		},
+		"602e93f58db767001e87948b": {
+			"id": "602e93f58db767001e87948b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"602e93f58db767001e87948d": {
+			"id": "602e93f58db767001e87948d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
